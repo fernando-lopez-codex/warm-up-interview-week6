357da6a2b7924dd62a879c8179bd77a048abd0cb
LaShaylaM35 <lashaylam@yahoo.com>
2026-01-09 00:28:55 -0600

    hid the api key

--- lashayla.js
+++ lashayla.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://nbwqtuymbktwrhmopjti.supabase.co/rest/v1/Pets_Table";
-const APIKEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5id3F0dXltYmt0d3JobW9wanRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTA3MDcsImV4cCI6MjA4MDM4NjcwN30.V6AO7DgIWRYIn8sHdWDjGVLQ0PSHAsl_6vyGKk0z2Qs"
+const APIKEY ="sb_secret_qPsOy4Arig_so3W4Be524Q_najcQxpH"
   
 
 async function getPets() {
